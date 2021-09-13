45a9f7a6d5696ad8e898aa2e7fe61d7520799d03
Jayden <ciebiesaraningo@gmail.com>
2021-09-13 17:08:56 +0800

    Initial Commit

---
+++ toDoList/toDoListApp.swift
@@ -0,0 +1,17 @@
+//
+//  toDoListApp.swift
+//  toDoList
+//
+//  Created by Jayden Lee on 13/9/21.
+//
+
+import SwiftUI
+
+@main
+struct toDoListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
